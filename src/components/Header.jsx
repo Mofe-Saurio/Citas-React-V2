03767df4210bdb75ff6245c42511a6ed2e628dcc
Mofeta <felrichdev@gmail.com>
2023-05-07 20:27:12 -0500

    new title

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header() {
   return (
     <h1 className="text-5xl font-black text-center md:w-1/2 mx-auto">
-      Seguimiento Pacientes {""}
+      Seguimiento Pacientes {""} con GitHub
       <span className="text-indigo-700">Veterinaria</span>
     </h1>
   );
